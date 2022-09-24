17680dd18c7ee4ededc4c2f3070c1307d8fa4021
Mehmet Özkan <75026832+mehmetozkn@users.noreply.github.com>
2022-09-24 23:36:03 +0300

    Mongoose done

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
 const path = require('path');
+const { response } = require('express');
+const Photo = require('./models/Photo.js');
 
 const app = express();
 
+ mongoose.connect('mongodb://localhost/pcat-test-db');
+
+
+// Template Engine
+app.set('view engine', 'ejs');
+
+// Middlewares
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', {
+    photos,
+  });
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
 });
 
 app.listen(port, () => {
