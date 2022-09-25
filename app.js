d26db2b1330032e86ce8adb39688ab0ad1875773
Mehmet Özkan <75026832+mehmetozkn@users.noreply.github.com>
2022-09-25 19:47:58 +0300

    Organized according to MVC structure

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+var methodOverride = require('method-override');
 const ejs = require('ejs');
-const path = require('path');
 const { response } = require('express');
-const Photo = require('./models/Photo.js');
+const photoController = require('./controllers/photoControllers');
+const pageController = require('./controllers/pageControllers');
 
 const app = express();
 
@@ -16,38 +18,25 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
+
+// ROUTES
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePhoto);
+
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
 
 const port = 3000;
-
-app.get('/', async (req, res) => {
-  const photos = await Photo.find({});
-  res.render('index', {
-    photos,
-  });
-});
-
-app.get('/photos/:id', async (req, res) => {
-  // console.log(req.params.id);
-  //  res.render('about');
-  const photo = await Photo.findById(req.params.id);
-  res.render('photo', {
-    photo
-  })
-});
-
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-
-app.get('/add', (req, res) => {
-  res.render('add');
-});
-
-app.post('/photos', async (req, res) => {
-  await Photo.create(req.body);
-  res.redirect('/');
-});
-
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı.`);
 });
